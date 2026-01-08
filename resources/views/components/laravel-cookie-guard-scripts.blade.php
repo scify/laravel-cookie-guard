e49691c3a1ae9b1881fe2d05853282f5e05dc9dc
Paul Isaris <paulisaris@gmail.com>
2026-01-08 12:00:25 +0200

    cache bust

--- resources/views/components/laravel-cookie-guard-scripts.blade.php
+++ resources/views/components/laravel-cookie-guard-scripts.blade.php
@@ -1,9 +1,9 @@
-<link rel="stylesheet" href="{{ asset('vendor/scify/laravel-cookie-guard/_variables.css') }}">
-<link rel="stylesheet" href="{{ asset('vendor/scify/laravel-cookie-guard/styles.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/scify/laravel-cookie-guard/_variables.css') }}?v={{ filemtime(public_path('vendor/scify/laravel-cookie-guard/_variables.css')) }}">
+<link rel="stylesheet" href="{{ asset('vendor/scify/laravel-cookie-guard/styles.css') }}?v={{ filemtime(public_path('vendor/scify/laravel-cookie-guard/styles.css')) }}">
 <script>
     window.cookies_consent_translations = {
         read_more: "{{ __('cookies_consent::messages.read_more') }}",
         read_less: "{{ __('cookies_consent::messages.read_less') }}"
     };
 </script>
-<script src="{{ asset('vendor/scify/laravel-cookie-guard/scripts.js') }}"></script>
+<script src="{{ asset('vendor/scify/laravel-cookie-guard/scripts.js') }}?v={{ filemtime(public_path('vendor/scify/laravel-cookie-guard/scripts.js')) }}"></script>
